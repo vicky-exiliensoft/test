a35025edc320fe61482adc97e153d0eedd28b876
vicky <vicky@exiliensoft.com>
2024-04-03 10:08:43 +0530

    improvement(change): url in axiosInstance.tsx

--- client/src/utility/axiosInstance.tsx
+++ client/src/utility/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000" ||  process.env.BASE_URL,
+  baseURL: "https://node-lbank.onrender.com" ||  process.env.BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
